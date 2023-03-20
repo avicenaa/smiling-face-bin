bc1176266580cfb059e286f933dfa1a3fc6f0106
Ilham Avicena <85075367+avicenaa@users.noreply.github.com>
2023-03-20 08:39:23 +0700

    Update smiling_face.ino

--- smiling_face.ino
+++ smiling_face.ino
@@ -4,16 +4,16 @@ Servo myservo;
 
 void setup() {
   Serial.begin(9600);
-  myservo.attach(9); // Pin servo motor terhubung ke pin 9
+  myservo.attach(9); // The servo motor pin is connected to pin 9
 }
 
 void loop() {
   if (Serial.available() > 0) {
     char data = Serial.read();
     if (data == '1') {
-      myservo.write(90); // Menggerakkan servo ke posisi tengah (90 derajat)
+      myservo.write(90); // moving the servo to centre position(90 degrees).
     } else if (data == '0') {
-      myservo.write(0); // Menggerakkan servo ke posisi awal (0 derajat)
+      myservo.write(0); // "Moving the servo to the initial position (0 degrees).
     }
   }
 }
